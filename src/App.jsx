0fbf81536a2a4dd03fdf4d3894d32313e76204a0
jezulin05 <jesrogo573@gmail.com>
2024-12-13 11:58:38 +0100

    segundo bloque entero

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import './App.css'
-import ButtonComponent from './components/ButtonComponent'
-import HeaderComponent from './components/HeaderComponent'
+import React, { useState } from 'react';
+import './App.css';
+import ButtonComponent from './components/ButtonComponent';
+import HeaderComponent from './components/HeaderComponent';
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const addOne = () => {
+    setNumber(number + 1);
+    console.log(number);
+  };
+
+  const sayHello = () => {
+    console.log("Hello");
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <>
-    <HeaderComponent></HeaderComponent>
-    <main className='main-content'>
-      <h2>Hola que tal</h2>
-      <ButtonComponent></ButtonComponent>
-    </main>
+      <HeaderComponent />
+      <main className="main-content">
+        <h2 onClick={sayHello}>Hola que tal</h2>
+        <h2 onClick={addOne}>Number: {number}</h2>
+        <input type="text" onChange={handleChange} />
+        <br />
+        <br />
+        <ButtonComponent />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
